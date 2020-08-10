f1baf343aae1a8801acf8af8add867407dd31a13
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-08-10 10:29:44 -0400

    cambios

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -23,48 +23,52 @@ class BannerController extends Controller
 
         try{
             $type = "";
-            try{
+            if($request->get('image') != null){
+                try{
 
-                $imageData = $request->get('image');
+                    $imageData = $request->get('image');
 
-                if(explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[0] == "video"){
+                    if(explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[0] == "video"){
+                        
+                        $data = explode( ',', $imageData);
+                        $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.'.explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
+                        $ifp = fopen($fileName, 'wb' );
+                        fwrite($ifp, base64_decode( $data[1] ) );
+                        rename($fileName, 'images/banners/'.$fileName);
+                        $type = "video";
+                    }
+        
+                    else if(strpos($imageData, "svg+xml") > 0){
+        
+                        $data = explode( ',', $imageData);
+                        $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.'."svg";
+                        $ifp = fopen($fileName, 'wb' );
+                        fwrite($ifp, base64_decode( $data[1] ) );
+                        rename($fileName, 'images/banners/'.$fileName);
+
+                        $type = "image";
+        
+                    }else{
+        
+                        $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.' . explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
+                        Image::make($request->get('image'))->save(public_path('images/banners/').$fileName);
+
+                        $type = "image";
+        
+                    }
+        
                     
-                    $data = explode( ',', $imageData);
-                    $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.'.explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
-                    $ifp = fopen($fileName, 'wb' );
-                    fwrite($ifp, base64_decode( $data[1] ) );
-                    rename($fileName, 'images/banners/'.$fileName);
-                    $type = "video";
+                }catch(\Exception $e){
+        
+                    return response()->json(["success" => false, "msg" => "Hubo un problema con la imagen", "err" => $e->getMessage(), "ln" => $e->getLine()]);
+        
                 }
-    
-                else if(strpos($imageData, "svg+xml") > 0){
-    
-                    $data = explode( ',', $imageData);
-                    $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.'."svg";
-                    $ifp = fopen($fileName, 'wb' );
-                    fwrite($ifp, base64_decode( $data[1] ) );
-                    rename($fileName, 'images/banners/'.$fileName);
-
-                    $type = "image";
-    
-                }else{
-    
-                    $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.' . explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
-                    Image::make($request->get('image'))->save(public_path('images/banners/').$fileName);
-
-                    $type = "image";
-    
-                }
-    
-                
-            }catch(\Exception $e){
-    
-                return response()->json(["success" => false, "msg" => "Hubo un problema con la imagen", "err" => $e->getMessage(), "ln" => $e->getLine()]);
-    
             }
 
             $banner = Banner::find(1);
-            $banner->image = $fileName;
+            if($request->get('image') != null){
+                $banner->image = $fileName;
+            }
             $banner->small_text = $request->smallText;
             $banner->big_text = $request->bigText;
             $banner->type = $type;
